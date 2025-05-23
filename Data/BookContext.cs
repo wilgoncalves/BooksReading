b7ae1c8e004fe6054aaa833436b603eb6ef97e34
Willian Gonçalves <santoswil81@gmail.com>
2025-05-23 15:53:44 -0300

    configurando banco de dados

---
+++ Data/BookContext.cs
@@ -0,0 +1,15 @@
+﻿using BookReadings.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookReadings.Data;
+
+public class BookContext : DbContext
+{
+    public DbSet<BookReadingsModel> Books { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlite("Data Source=booksreading.sqlite");
+        base.OnConfiguring(optionsBuilder);
+    }
+}
--- obj/Debug/net9.0/BookReadings.AssemblyInfo.cs
+++ obj/Debug/net9.0/BookReadings.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BookReadings")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+18c1bd4622285626d655d61e6762d7c03b394665")]
 [assembly: System.Reflection.AssemblyProductAttribute("BookReadings")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BookReadings")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
