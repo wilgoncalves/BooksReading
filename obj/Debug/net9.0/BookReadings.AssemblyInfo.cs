73429456baa263c70f98c2d8e0cbea3aef3121e2
Willian Gonçalves <122633904+wilgoncalves@users.noreply.github.com>
2026-03-12 09:24:40 -0300

    Adicionando novos titulos

--- obj/Debug/net9.0/BookReadings.AssemblyInfo.cs
+++ obj/Debug/net9.0/BookReadings.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BookReadings")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+472ca9fe761cddbf8d0d287ad177f8f13fc9a90d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+834b6b3e9cb2b9420ad4c594e868ba8906db2ea6")]
 [assembly: System.Reflection.AssemblyProductAttribute("BookReadings")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BookReadings")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
